ef636b4df3db85e1bc4cf73322796c1ae4547550
Didac <ediego918@gmail.com>
2023-05-22 21:06:25 -0400

    se agrega el proyecto

---
+++ src/holagit/mardown/main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holagit.mardown;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author HP
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("HOLA Git+Mardown");
+        JOptionPane.showMessageDialog(null, "HOLA Git+Mardown");
+        
+    }
+}
